8b761cdd5b832522f469a64113a48a19e535cf5a
Developer-mayur <ec21.mayurrathore@svceindore.ac.in>
2025-02-03 17:35:46 +0530

    new

--- Test/project/routers/studentRouter.js
+++ Test/project/routers/studentRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Handle /student route
-router.get('/', (req, res) => {
-  res.render('student/home'); // Make sure the view exists
+ router.get('/', (req, res) => {
+  res.render('student/home');  
 });
 
 module.exports = router;
